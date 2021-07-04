60b399ca2e8ab2a3b199438f38b959e5ad2a01a4
EugeneLadyka <eugeneladyka@gmail.com>
2021-07-04 10:59:59 +0300

    Step-6 test selemium

---
+++ venv/lib/python3.8/lesson6/lesson6_step6_for_in.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/huge_form.html"
+
+try:
+    driver = webdriver.Chrome()
+    driver.get(link)
+
+    elements = driver.find_elements_by_tag_name("input")
+    for element in elements:
+        element.send_keys("Ronny")
+
+    button = driver.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(20)
+    driver.quit()
