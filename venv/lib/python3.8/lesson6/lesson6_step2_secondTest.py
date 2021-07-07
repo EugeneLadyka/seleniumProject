f9c5d49e4e73601436748b4476bf145acfadb452
EugeneLadyka <65681999+EugeneLadyka@users.noreply.github.com>
2021-07-07 23:15:03 +0300

    Delete venv/lib/python3.8/lesson6 directory

--- venv/lib/python3.8/lesson6/lesson6_step2_secondTest.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("http://suninjuly.github.io/simple_form_find_task.html")
-button = driver.find_element_by_id("submit_button")
-
-# закрываем браузер после всех манипуляций
-driver.quit()
